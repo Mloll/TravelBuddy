43ce9bbb992c7fc6416384e286e914e7c8e4495f
viktorstrate <viktorstrate@gmail.com>
2025-02-28 13:59:47 +0100

    initial commit

---
+++ TravelBuddy/TravelBuddyApp.swift
@@ -0,0 +1,43 @@
+//
+//  TravelBuddyApp.swift
+//  TravelBuddy
+//
+//  Created by Viktor Strate Kløvedal on 28/02/2025.
+//
+
+import SwiftUI
+import SwiftData
+import UniformTypeIdentifiers
+
+@main
+struct TravelBuddyApp: App {
+    var body: some Scene {
+        DocumentGroup(editing: .itemDocument, migrationPlan: TravelBuddyMigrationPlan.self) {
+            ContentView()
+        }
+    }
+}
+
+extension UTType {
+    static var itemDocument: UTType {
+        UTType(importedAs: "com.example.item-document")
+    }
+}
+
+struct TravelBuddyMigrationPlan: SchemaMigrationPlan {
+    static var schemas: [VersionedSchema.Type] = [
+        TravelBuddyVersionedSchema.self,
+    ]
+
+    static var stages: [MigrationStage] = [
+        // Stages of migration between VersionedSchema, if required.
+    ]
+}
+
+struct TravelBuddyVersionedSchema: VersionedSchema {
+    static var versionIdentifier = Schema.Version(1, 0, 0)
+
+    static var models: [any PersistentModel.Type] = [
+        Item.self,
+    ]
+}
---
+++ TravelBuddyTests/TravelBuddyTests.swift
@@ -0,0 +1,16 @@
+//
+//  TravelBuddyTests.swift
+//  TravelBuddyTests
+//
+//  Created by Viktor Strate Kløvedal on 28/02/2025.
+//
+
+import Testing
+
+struct TravelBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
